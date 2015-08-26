8b88af569db75d0809b9f1c429d333b1d150bb51
ansidev <ansidev@gmail.com>
2015-08-26 16:47:09 +0700

    Update content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,22 +18,22 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Title</a>
+			<a class="navbar-brand" href="/">Hermit Blog</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Link</a></li>
-				<li><a href="#">Link</a></li>
+				<!-- <li class="active"><a href="#">Link</a></li> -->
+				<!-- <li><a href="#">Link</a></li> -->
 			</ul>
-			<form class="navbar-form navbar-left" role="search">
-				<div class="form-group">
-					<input type="text" class="form-control" placeholder="Search">
-				</div>
-				<button type="submit" class="btn btn-default">Submit</button>
-			</form>
-			<ul class="nav navbar-nav navbar-right">
+			<!-- <form class="navbar-form navbar-left" role="search"> -->
+				<!-- <div class="form-group"> -->
+					<!-- <input type="text" class="form-control" placeholder="Tìm kiếm"> -->
+				<!-- </div> -->
+				<!-- <button type="submit" class="btn btn-default">Search</button> -->
+			<!-- </form> -->
+			<!-- <ul class="nav navbar-nav navbar-right">
 				<li><a href="#">Link</a></li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
@@ -44,19 +44,19 @@
 						<li><a href="#">Separated link</a></li>
 					</ul>
 				</li>
-			</ul>
+			</ul> -->
 		</div><!-- /.navbar-collapse -->
 	</nav>
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="jumbotron">
 					<div class="container">
-						<h1>Hello, world!</h1>
-						<p>Contents ...</p>
-						<p>
+						<h1>Website đang trong quá trình xây dựng</h1>
+						<p>Bạn vui lòng quay lại sau.</p>
+						<!-- <p>
 							<a class="btn btn-primary btn-lg">Learn more</a>
-						</p>
+						</p> -->
 					</div>
 				</div>
 			</div>
