b39c738ebe1b2d7af8153ef22a0dfa97f4c3e753
ansidev <ansidev@gmail.com>
2015-09-03 17:37:12 +0700

    Update homepage content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,31 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Hermit Blog</title>
+	<link href="{{ asset('/css/font-awesome.min.css') }}" rel="stylesheet">
+
 	<!-- Latest compiled and minified CSS & JS -->
 	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<script src="//code.jquery.com/jquery.js"></script>
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-9">
+				<h1>Hermit Blog</h1>
+				<p>Blog của ansidev.</p>
+			</div>
+			<div class="col-md-3">
+				<div class="well" style="margin-top:20px">
+					<div class="container">
+						<p>CONTACT INFO</p>
+						<p><i class="fa fa-envelope-o"></i> <a href="mailto:ansidev@gmil.com">ansidev@gmail.com</a></p>
+						<p><i class="fa fa-github"></i> <a href="https://github.com/ansidev">ansidev</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<nav class="navbar navbar-default" role="navigation">
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
@@ -27,37 +46,80 @@
 				<!-- <li class="active"><a href="#">Link</a></li> -->
 				<!-- <li><a href="#">Link</a></li> -->
 			</ul>
-			<!-- <form class="navbar-form navbar-left" role="search"> -->
-				<!-- <div class="form-group"> -->
-					<!-- <input type="text" class="form-control" placeholder="Tìm kiếm"> -->
-				<!-- </div> -->
-				<!-- <button type="submit" class="btn btn-default">Search</button> -->
-			<!-- </form> -->
-			<!-- <ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Link</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li><a href="#">Separated link</a></li>
-					</ul>
-				</li>
-			</ul> -->
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<!-- <button type="submit" class="btn btn-default">Submit</button> -->
+			</form>
 		</div><!-- /.navbar-collapse -->
 	</nav>
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="jumbotron">
 					<div class="container">
 						<h1>Website đang trong quá trình xây dựng</h1>
 						<p>Bạn vui lòng quay lại sau.</p>
-						<!-- <p>
-							<a class="btn btn-primary btn-lg">Learn more</a>
-						</p> -->
 					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-9">
+				<div class="col-md-4">
+					<h2>Article #1</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ullam ratione dolore sint illum placeat architecto recusandae ducimus? Iste expedita consectetur accusantium tempora quasi, laudantium ut ipsam omnis debitis repellendus adipisci unde ducimus consequatur qui at neque, odio eius nam.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #2</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci inventore autem nemo rerum eveniet dolore natus, quisquam nesciunt earum alias, accusamus esse voluptatem aliquid! Ut facilis porro saepe, laboriosam animi sequi voluptatibus, suscipit sapiente neque, quam illum. Fugiat quidem, alias.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #3</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ullam ratione dolore sint illum placeat architecto recusandae ducimus? Iste expedita consectetur accusantium tempora quasi, laudantium ut ipsam omnis debitis repellendus adipisci unde ducimus consequatur qui at neque, odio eius nam.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #4</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci inventore autem nemo rerum eveniet dolore natus, quisquam nesciunt earum alias, accusamus esse voluptatem aliquid! Ut facilis porro saepe, laboriosam animi sequi voluptatibus, suscipit sapiente neque, quam illum. Fugiat quidem, alias.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #5</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ullam ratione dolore sint illum placeat architecto recusandae ducimus? Iste expedita consectetur accusantium tempora quasi, laudantium ut ipsam omnis debitis repellendus adipisci unde ducimus.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #6</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci inventore autem nemo rerum eveniet dolore natus, quisquam nesciunt earum alias, accusamus esse voluptatem aliquid! Ut facilis porro saepe, laboriosam animi sequi voluptatibus, suscipit sapiente neque, quam illum. Fugiat quidem, alias.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #7</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore ullam ratione dolore sint illum placeat architecto recusandae ducimus? Iste expedita consectetur accusantium tempora quasi, laudantium ut ipsam omnis debitis repellendus adipisci unde ducimus consequatur qui at neque, odio eius nam.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #8</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci inventore autem nemo rerum eveniet dolore natus, quisquam nesciunt earum alias, accusamus esse voluptatem aliquid! Ut facilis porro saepe, laboriosam animi sequi voluptatibus, suscipit sapiente neque, quam illum. Fugiat quidem, alias.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+				<div class="col-md-4">
+					<h2>Article #9</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci inventore autem nemo rerum eveniet dolore natus, quisquam nesciunt earum alias, accusamus esse voluptatem aliquid! Ut facilis porro saepe, laboriosam animi sequi voluptatibus, suscipit sapiente neque, quam illum. Fugiat quidem, alias.</p>
+					<p><a href="#">Read more</a></p>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="well">
+					<h1>Sidebar</h1>
+					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, quas!</p>
 				</div>
 			</div>
 		</div>
