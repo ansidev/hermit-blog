b7c87fe98ef140db781f8c5dfe4cd99609852067
Le Minh Tri <ansidev@gmail.com>
2015-09-08 16:32:04 +0700

    Add code for display error messages

--- resources/views/items/item_edit.blade.php
+++ resources/views/items/item_edit.blade.php
@@ -3,6 +3,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             {!! Form::model($item, [
                     'route' => [ 'items.update', $item->id ],
                     'method' => 'PUT',
