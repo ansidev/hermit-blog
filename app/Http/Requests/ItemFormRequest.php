0354f0a7bbada29ff99354bc35976cea973f9671
Le Minh Tri <ansidev@gmail.com>
2015-09-10 11:38:00 +0700

    Disable rules for fields `parent_name` and `childable`.

--- app/Http/Requests/ItemFormRequest.php
+++ app/Http/Requests/ItemFormRequest.php
@@ -24,10 +24,10 @@ class ItemFormRequest extends Request
         return [
         	'name' => 'required',
         	'parent_id' => 'required|numeric',
-        	'parent_name' => 'alpha',
+        	// 'parent_name' => 'alpha',
         	'location_id' => 'numeric',
         	// 'location_name' => 'regex:/^[A-Za-z0-9 ]+$/',
-        	'childable' => 'boolean'
+            // 'childable' => 'boolean',
     	];
     }
 }
